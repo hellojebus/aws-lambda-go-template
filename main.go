b9bfacf6d1eed1dbc900dcca9dee009475f356a6
hellojebus <dev@hellojeb.us>
2018-11-17 15:54:47 -0800

    add basic app struct and route

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/thedevsaddam/renderer"
+)
+
+var rnd *renderer.Render
+
+func init() {
+	opts := renderer.Options{
+		ParseGlobPattern: "./tpl/*.html",
+	}
+	rnd = renderer.New(opts)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/about", about)
+	port := ":9000"
+	log.Println("Listening on port ", port)
+	http.ListenAndServe(port, mux)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	rnd.HTML(w, http.StatusOK, "Home", nil)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	rnd.HTML(w, http.StatusOK, "About", nil)
 }
