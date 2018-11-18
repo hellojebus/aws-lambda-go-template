3a36b28948ea5f6e70be6774e0d1cc325c35b978
hellojebus <dev@hellojeb.us>
2018-11-18 09:37:48 -0800

    fix typo

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var port string = ":8080"
+
 func setupRouter(router *mux.Router) {
 	router.
 		Methods("POST").
@@ -14,9 +16,12 @@ func setupRouter(router *mux.Router) {
 		HandlerFunc(postFunction)
 }
 
-func postFunction(w http.ResponseWriter, r *response.Request) {
+func postFunction(w http.ResponseWriter, r *http.Request) {
 	log.Println("post route called")
 }
 
 func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	setupRouter(router)
+	log.Fatal(http.ListenAndServe(port, router))
 }
