95f2968fd2f299c4c5ce323c5d9ef466d7241f37
hellojebus <dev@hellojeb.us>
2018-11-18 09:31:10 -0800

    add post route

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-func init() {
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func setupRouter(router *mux.Router) {
+	router.
+		Methods("POST").
+		Path("/endpoint").
+		HandlerFunc(postFunction)
+}
+
+func postFunction(w http.ResponseWriter, r *response.Request) {
+	log.Println("post route called")
 }
 
 func main() {
