976b74c9c502ad7aa3c7f312d0b9ca038b15e23c
hellojebus <dev@hellojeb.us>
2018-11-17 21:34:38 -0800

    remove prev version

--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/thedevsaddam/renderer"
-)
-
-var rnd *renderer.Render
-
 func init() {
-	opts := renderer.Options{
-		ParseGlobPattern: "./tpl/*.html",
-	}
-	rnd = renderer.New(opts)
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/about", about)
-	port := ":9000"
-	log.Println("Listening on port ", port)
-	http.ListenAndServe(port, mux)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("home triggered")
-	rnd.HTML(w, http.StatusOK, "home", nil)
-}
-
-func about(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("about triggered")
-	rnd.HTML(w, http.StatusOK, "about", nil)
 }
